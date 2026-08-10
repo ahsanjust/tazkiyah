16502e0b225bc136bed716450418ec87feda7e43
Ahsanul Haque <ahsanulhaque5588@gmail.com>
2026-08-10 09:21:54 +0600

    public release: only PDFs + website + README on main; sources moved to source branch

--- scripts/clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Cleaning auxiliary LaTeX files..."
-
-# Find all auxiliary files recursively and delete them
-find . -type f \( -name "*.aux" -o -name "*.log" -o -name "*.out" -o -name "*.toc" -o -name "*.fls" -o -name "*.fdb_latexmk" \) -delete
-
-echo "Clean complete!"
